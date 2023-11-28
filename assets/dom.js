90e3900feefe1785594737e912201ee5511c66c8
Felipe de Lima Jordão <DUQ0288451@aluno.firjansenaisesi.com.br>
2023-11-28 11:39:49 -0300

    first commit

---
+++ assets/dom.js
@@ -0,0 +1,25 @@
+let produtos = [
+    {
+        "nome": 'Chocker Coração de Prata'
+        "preço": 'R$ 22.00'
+        "imagem": 'assets/img/Cordão Coração'
+    }
+
+    {
+        "nome": 'Tornozeleira Sereia Prata'
+        "preço": 'R$ 15.00'
+        "imagem": 'assets/img/Cordão Coração'
+    }
+
+    {
+        "nome": 'Cordão Gravatinha com Letra'
+        "preço": 'R$ 20.00'
+        "imagem": 'assets/img/Cordão Coração'
+    }
+
+    {
+        "nome": 'Chocker Coração de Prata'
+        "preço": 'R$ 22.00'
+        "imagem": 'assets/img/Cordão Coração'
+    }
+]
