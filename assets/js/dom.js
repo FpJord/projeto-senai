3c2288a25243919864c9380abb528398c1df4b70
FpJord <delimafelipe59@gmail.com>
2023-12-19 15:25:25 -0300

    ok?

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -2,41 +2,41 @@ let listaProdutos = [
     {
         "nome": 'Chocker Coração de Prata',
         "preço": "R$ 22.00",
-        "imagem": "assets/img/Cordão Coração.svg",
+        "imagem": "assets/img/Chocker Coração.svg",
     },
 
     {
         "nome": 'Tornozeleira Sereia Prata',
         "preço": 'R$ 15.00',
-        "imagem": "assets/img/sereia prata.svg",
+        "imagem": "assets/img/Tornozeleira Sereia.svg",
     },
 
     {
         "nome": 'Cordão Gravatinha com Letra',
         "preço": 'R$ 20.00',
-        "imagem": "assets/img/gravatinha l.svg",
+        "imagem": "assets/img/Cordão Gravatinha L.svg",
     },
 
     {
         "nome": 'Chocker Coração Dourado',
         "preço": 'R$ 22.00',
-        "imagem": 'assets/img/coração dourado.svg',
+        "imagem": 'assets/img/Chocker Dourada.svg',
     },
 
     {
         "nome": 'Argola P Prata e Argola P Dourada',
         "preço": 'R$ 7.00',
-        "imagem": 'assets/img/argola p e m.svg',
+        "imagem": 'assets/img/Argola P e M.svg',
     },
 
     {
         "nome": 'Brincos Pequenos, Vários Modelos',
         "preço": 'R$ 3.00',
-        "imagem": 'assets/img/brincos pequenos.svg',
+        "imagem": 'assets/img/Brincos Pequenos.svg',
     },
 ];
 
-listaProdutos.map((produto) => {
+listaProdutos.map((produto, posicao) => {
     let cardProduto = document.getElementById("first-dom");
 
     cardProduto.innerHTML += `
@@ -46,7 +46,86 @@ listaProdutos.map((produto) => {
     <h2>${produto.nome}</h2>
     <span>${produto.preço}</span>
 
-    <a href="#"><Button>Comprar</Button></a>
+    <Button class="teste">Comprar</Button>
 </div>
 `
-})
+
+console.log(posicao)
+});
+
+
+
+let listaRecomendancao = [
+    {
+        "nome": 'Cordão Pingente Menina',
+        "preço": 'R$ 20.00',
+        "imagem": 'assets/img/Pingente Menina.svg'
+        
+    },
+
+    {
+        "nome": 'Cordão de Aço Duplo Corações',
+        "preço": 'R$ 27.00',
+        "imagem": 'assets/img/Cordão Duplo Corações.svg'
+        
+    },
+    
+    {
+        "nome": 'Chocker Escama de Peixe Dourada',
+        "preço": 'R$ 22.00',
+        "imagem": 'assets/img/Chocker Escama de Peixe.svg'
+        
+    },
+    
+    {
+        "nome": 'Cordão Coração com Ponto de Luz Dourado',
+        "preço": 'R$ 22.00',
+        "imagem": 'assets/img/Ponto de Luz Dourado.svg'
+        
+    },
+    
+    {
+        "nome": 'Argola M Prata',
+        "preço": 'R$ 8.00',
+        "imagem": 'assets/img/Argola M.svg'
+        
+    },
+    
+    {
+        "nome": 'Argola M G Prata',
+        "preço": 'R$ 10.00',
+        "imagem": 'assets/img/Argola G.svg'
+        
+    },
+    
+];
+
+listaRecomendancao.map((produto, posicao) => {
+    let cardRecomendacao = document.getElementById("second-dom");
+
+    cardRecomendacao.innerHTML += `
+    <div class="product-card">
+    <img src="${produto.imagem}" alt="" id="product-image">
+    
+    <h2>${produto.nome}</h2>
+    <span>${produto.preço}</span>
+
+    <Button class="teste" onClick="zoomRecomendacao('${posicao}')">Comprar</Button>
+</div>
+`
+
+console.log(posicao)
+});
+
+function zoomRecomendacao(posicao){
+    let recSelecionado = listaRecomendancao[posicao];
+    document.getElementById("nome-produto").innerHTML = recSelecionado.nome;
+    document.getElementById("imagem-produto").src = recSelecionado.imagem;
+
+    new boostrap.Modal('#zoomProduct').show();
+
+}
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,44 @@
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-buttton-prev",
-    },
+/* Modal */
+
+const openModal = document.querySelector(".anel");
+const closeModal = document.querySelector("#fechar");
+const modal = document.querySelector("#modal-produto");
+const fade = document.querySelector("#fade");
+
+
+
+
+
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const number = document.querySelector(".number");
+const value = document.querySelector("#value-number");
+
+
+let a = 1;
+number.innerHTML = a;
+
+let b = 22;
+value.innerHTML = b;
+
+
+plus.addEventListener("click", ()=>{
+    a++;
+
+    number.innerText = a;
+
+    value.innerText = b * a;
 });
+
+minus.addEventListener("click", ()=> {
+   if(a > 1){
+    a--;
+    number.innerText = a;
+
+    value.innerText = (b * a);
+   } 
+});
+
+
+
+console.log(value.value);
